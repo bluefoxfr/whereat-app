864ea39f052acbbbb5cb9bcb9e7e278e7d627a0d
fgrimaepitech <florian.grima@epitech.eu>
2022-07-20 17:42:08 +0200

    feat: add 404 error page

---
+++ src/pages/404.tsx
@@ -0,0 +1,42 @@
+import Link from 'next/link'
+import styled from 'styled-components';
+
+const ErrorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+`
+
+const ErrorTitle = styled.h1`
+  flex: 0;
+  font-family: 'Poppins';
+  font-style: italic;
+  font-weight: 700;
+  font-size: 96px;
+  line-height: 144px;
+  color: #1D1D1D;
+`
+
+const ErrorDesc = styled.h1`
+  flex: 0;
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 40px;
+  line-height: 60px;
+  color: #1D1D1D;
+  margin-top: -20px;
+`
+
+export default function FourOhFour() {
+  return <>
+    <ErrorContainer>
+      <ErrorTitle>Error 404</ErrorTitle>
+      <ErrorDesc>Page not found</ErrorDesc>
+    </ErrorContainer>
+  </>
+}
